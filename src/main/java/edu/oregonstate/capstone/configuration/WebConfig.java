899e8b8db3482417d7bc156afd21e1163f4b68c6
mikeacosta <mike@acosta.net>
2022-11-01 10:36:34 -0700

    allow add'l CORS methods

--- src/main/java/edu/oregonstate/capstone/configuration/WebConfig.java
+++ src/main/java/edu/oregonstate/capstone/configuration/WebConfig.java
@@ -9,7 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST","PUT", "DELETE");
     }
 
     @Override
